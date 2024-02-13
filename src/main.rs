d691b0386d68c6bb84959f90b1b75d5218e3ca10
Carlos Alonso Martín <sr.titere@gmail.com>
2024-02-13 12:30:09 +0100

    feat(json): add json format and filter v6 and v4 ip version.

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,29 @@ use std::env;
 use colored::*;
 use pnet::datalink; // Importa todo desde colored
 use serde::{Deserialize, Serialize};
-use serde_json;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() > 1 {
         match args[1].as_str() {
-            "json" => json_mode(),
+            "json" | "-j" | "j" => {
+                if args.len() > 2 {
+                    match args[2].as_str() {
+                        "v4" | "4" => json_mode_v4(),
+                        "v6" | "6" => json_mode_v6(),
+                        _ => json_mode(),
+                    }
+                } else {
+                    json_mode()
+                }
+            }
+
             _ => standard(),
         };
-    }
+    } else {
+        standard();
+    };
 }
 
 fn standard() {
@@ -26,8 +38,8 @@ fn standard() {
         .into_iter()
         .filter(|e| e.is_up() && !e.ips.is_empty())
     {
-        println!("{}: {}", "Nombre".green(), iface.name.yellow());
-        println!("{}", "IPs:".green());
+        println!("{}: {}", "IfName".green(), iface.name.yellow());
+        println!("{}", "Address:".green());
         for ip in iface.ips {
             println!(" - {}", ip.to_string().blue());
         }
@@ -43,6 +55,7 @@ struct Info {
 
 fn json_mode() {
     let all_interfaces = datalink::interfaces();
+    let mut info: Vec<Info> = vec![];
     for iface in all_interfaces
         .into_iter()
         .filter(|e| e.is_up() && !e.ips.is_empty())
@@ -51,6 +64,50 @@ fn json_mode() {
             ifname: iface.name,
             addrs: iface.ips.iter().map(|e| e.to_string()).collect(),
         };
-        println!("{}", serde_json::to_string(&json).unwrap());
+        info.push(json);
     }
+    println!("{}", serde_json::to_string(&info).unwrap());
+}
+// create a function to print the interface in json with only ip version 6
+fn json_mode_v6() {
+    let all_interfaces = datalink::interfaces();
+    let mut info: Vec<Info> = vec![];
+    for iface in all_interfaces
+        .into_iter()
+        .filter(|e| e.is_up() && !e.ips.is_empty())
+    {
+        let json = Info {
+            ifname: iface.name,
+            addrs: iface
+                .ips
+                .iter()
+                .filter(|e| e.is_ipv6())
+                .map(|e| e.to_string())
+                .collect(),
+        };
+        info.push(json);
+    }
+    println!("{}", serde_json::to_string(&info).unwrap());
+}
+
+// create a function to print the interface in json with only ip version 4
+fn json_mode_v4() {
+    let all_interfaces = datalink::interfaces();
+    let mut info: Vec<Info> = vec![];
+    for iface in all_interfaces
+        .into_iter()
+        .filter(|e| e.is_up() && !e.ips.is_empty())
+    {
+        let json = Info {
+            ifname: iface.name,
+            addrs: iface
+                .ips
+                .iter()
+                .filter(|e| e.is_ipv4())
+                .map(|e| e.to_string())
+                .collect(),
+        };
+        info.push(json);
+    }
+    println!("{}", serde_json::to_string(&info).unwrap());
 }
